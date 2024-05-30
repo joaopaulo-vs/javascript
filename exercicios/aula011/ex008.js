ce081377b32c59d2d10795e461dcc86e6c9c7297
João Paulo Vieira <58373975+joaopaulo-vs@users.noreply.github.com>
2024-05-30 03:50:30 -0300

    condições if else

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.8
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60.0){
+    console.log('Dirija sempre usando cinto de segurança')
+}
+console.log('Você ultrapassou a velocidade permitida. MULTADO')
